83ad927a36e6c46bbbf7323b57ea07e484c493a8
Dmitry1964 <nosdm64@gmail.com>
2024-11-20 14:42:28 +0300

    feat: добавляет хеадер

--- src/features/header/header.tsx
+++ src/features/header/header.tsx
@@ -2,16 +2,21 @@ import styles from './header.module.scss';
 const Header = () => (
   <header className={styles.header}>
     <div className={styles.header__avatar}>
-        <img src="content/avatar.jpg" alt="" srcSet="content/avatar@2x.jpg" />
+      <img src="content/avatar.jpg" alt="" srcSet="content/avatar@2x.jpg" />
     </div>
     <div className={styles.header__content}>
       <div className={styles.header__border}>
-        <div id='first'></div>
-        <div id='second'></div>
-        <div id='third'></div>
+        <div id="first"></div>
+        <div id="second"></div>
+        <div id="third"></div>
       </div>
+      <h2 className={styles.header__title}>Дмитрий М. Нос</h2>
+      <ul className={styles.header__list}>
+        <li>HTML-верстальщик</li>
+        <li>Frontend developer</li>
+        <li>React developer</li>
+      </ul>
     </div>
-  <p>kdfkdkfdkfdk</p>
   </header>
 );
 
